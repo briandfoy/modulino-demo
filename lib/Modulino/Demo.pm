84fa0bd5f10e12e217da5b3e1b30cf57065a07a0
brian d foy <brian.d.foy@gmail.com>
2012-05-28 10:59:48 -0800

    Start Modulino::Demo

---
+++ lib/Modulino/Demo.pm
@@ -0,0 +1,71 @@
+package Modulino::Demo;
+use strict;
+
+use warnings;
+no warnings;
+
+use subs qw();
+use vars qw($VERSION);
+
+$VERSION = '0.10_01';
+
+=encoding utf8
+
+=head1 NAME
+
+Modulino::Demo - XXX: This is the description
+
+=head1 SYNOPSIS
+
+	use Modulino::Demo;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new
+	{
+	
+	}
+	
+=item init
+
+=cut
+
+sub init
+	{
+	
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/modulino-demo/
+
+=head1 AUTHOR
+
+, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+
+Copyright (c) 2012, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ t/load.t
@@ -0,0 +1,10 @@
+BEGIN {
+	@classes = qw(Modulino::Demo);
+	}
+
+use Test::More tests => scalar @classes;
+
+foreach my $class ( @classes )
+	{
+	print "Bail out! $class did not compile\n" unless use_ok( $class );
+	}
