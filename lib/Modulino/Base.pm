cd5a1ce81bc37779db87f509e0c3165c8a65eaa8
brian d foy <brian.d.foy@gmail.com>
2013-08-20 13:32:36 -0500

    Fix Base.pm to work. It was easier than I thought.

--- lib/Modulino/Base.pm
+++ lib/Modulino/Base.pm
@@ -1,7 +1,7 @@
 package Modulino::Base;
 use utf8;
 use strict;
-use warnings;
+no warnings;
 
 use v5.10.1;
 
@@ -10,31 +10,36 @@ use Carp;
 
 our $VERSION = '0.10_01';
 
-sub _running_under_tester {
-	!! $ENV{CPANTEST}
-	}
+sub _running_under_tester { !! $ENV{CPANTEST} }
 
 sub _running_as_app {
-	defined scalar caller;
+	my $caller = scalar caller(1);
+	print "in _running_as_app, caller is $caller\n";
+	(defined $caller) && $caller ne 'main';
 	}
 
-my @caller = caller();
-
-if( $caller[0] ) {
+# run directly
+if( ! defined caller(0) ) {
+	carp "You cannot run this module directly\n";
+	}
+# loaded from a module that was run directly
+elsif( ! defined caller(1) ) {
+	my @caller = caller(0);
+	print "caller is [@caller]\n";
 	my $method = do {
 		   if( _running_under_tester()    ) { 'test' }
 		elsif( _running_as_app()          ) { 'run'  }
 		else                                { undef  }
 		};
 
-	unless( $caller[0]->can( $method ) ) {
-		carp "There is no $method() method defined in $caller[0]";	
-		}
+	if( defined $method ) {
+		unless( $caller[0]->can( $method ) ) {
+			carp "There is no $method() method defined in $caller[0]\n";	
+			}
 
-	$caller[0]->$method(@ARGV) if defined $method;
+		$caller[0]->$method(@ARGV) if defined $method;
+		}	
 	}
-else {
-	warn "You have to load Modulino::Base from another module";
-	}
+
 
 1;
