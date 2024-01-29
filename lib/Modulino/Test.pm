84dca21881566666aff1ea0df8eb7128099a4c65
brian d foy <briandfoy@pobox.com>
2024-01-28 20:24:39 -0500

    Update email

--- lib/Modulino/Test.pm
+++ lib/Modulino/Test.pm
@@ -138,11 +138,11 @@ This source is in Github:
 
 =head1 AUTHOR
 
-brian d foy, C<< <bdfoy@cpan.org> >>
+brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2013-2023, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2013-2023, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
