7a4411e0a5fed2ad9c852f8d36a937676b9fda7d
brian d foy <briandfoy@pobox.com>
2024-01-28 20:25:38 -0500

    Update copyright year

--- lib/Modulino/Demo.pm
+++ lib/Modulino/Demo.pm
@@ -165,7 +165,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2012-2023, brian d foy <briandfoy@pobox.com>. All rights reserved.
+Copyright © 2012-2024, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms as the Artistic License 2.0.
 
--- lib/Modulino/Demo2.pm
+++ lib/Modulino/Demo2.pm
@@ -162,7 +162,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2012-2023, brian d foy <briandfoy@pobox.com>. All rights reserved.
+Copyright © 2012-2024, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
